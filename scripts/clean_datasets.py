712360b75ffd395dce843f4ddd3517c1fb4ff153
jadenpinto <jadenclivepinto@gmail.com>
2025-02-09 17:28:49 +0000

    Filter output metadata for CS outputs and write to CSV

--- scripts/clean_datasets.py
+++ scripts/clean_datasets.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 
-from utils.constants import DATASETS_DIR, REF2021_EXPORTED_DIR, CS_UOA_RESULTS
+from utils.constants import DATASETS_DIR, REF2021_EXPORTED_DIR, CS_UOA_RESULTS, OUTPUTS_METADATA, REF2021_CLEANED_DIR, \
+    CS_OUTPUTS_METADATA
 
 def clean_results_dataset():
     # Define the Excel file name
@@ -27,4 +28,31 @@ def clean_results_dataset():
     except Exception as e:
         print("An error occurred while reading the file:", str(e))
 
-clean_results_dataset()
+def clean_outputs_dataset():
+    # Define the Excel file name
+    outputs_dataset_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, REF2021_EXPORTED_DIR, OUTPUTS_METADATA)
+
+    try:
+        # Load the Excel file, skipping the first 6 lines
+        # The 4th line will be used as the header
+        df = pd.read_excel(outputs_dataset_path, skiprows=4) # (185286, 42)
+        #print(df.head().to_string())
+        #print(df.columns)
+
+        cs_outputs = df[df['Unit of assessment number'] == 11] # (7296, 42)
+        print(cs_outputs.head().to_string())
+        print(cs_outputs.shape) # print("\nNumber of rows and columns:", df.shape)
+        # print(cs_outputs.columns)
+
+        return cs_outputs
+
+    except FileNotFoundError:
+        print("File not found. Please make sure the file name and path are correct.")
+    except Exception as e:
+        print("An error occurred while reading the file:", str(e))
+
+# clean_results_dataset() # for testing, make it return a df [and write to datasets], possible give argument
+cs_outputs = clean_outputs_dataset()
+
+cleaned_cs_outputs_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, REF2021_CLEANED_DIR, CS_OUTPUTS_METADATA)
+cs_outputs.to_csv(cleaned_cs_outputs_path) #, index=False)
--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,9 @@
 # Directories
 DATASETS_DIR = "datasets"
 REF2021_EXPORTED_DIR = "REF2021_Exported"
+REF2021_CLEANED_DIR = "REF2021_Cleaned"
 
 # Files
 CS_UOA_RESULTS =  "CS_UoA_Results.xlsx"
+OUTPUTS_METADATA = "All_Outputs_Metadata.xlsx"
+CS_OUTPUTS_METADATA = "CS_Outputs_Metadata.csv"
