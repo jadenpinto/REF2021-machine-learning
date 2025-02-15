25fa38dab265523b974c5b7fde993948cd4092a7
jadenpinto <jadenclivepinto@gmail.com>
2025-02-15 18:55:51 +0000

    Refactor SJR df - add filter_sjr_columns()

--- scripts/process_scimago_journal_rank.py
+++ scripts/process_scimago_journal_rank.py
@@ -5,7 +5,7 @@ from utils.constants import DATASETS_DIR, IMPACT_FACTOR_EXPORTED_DIR, SCIMAGO_JO
     SCIMAGO_JOURNAL_RANK_CLEANED, REF2021_CLEANED_DIR, CS_OUTPUTS_METADATA
 from utils.dataframe import log_dataframe, delete_rows_by_values
 
-def clean_sjr_dataset():
+def read_sjr_dataset():
     # Path to SCImago journal rank file
     sjr_dataset_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, IMPACT_FACTOR_EXPORTED_DIR, SCIMAGO_JOURNAL_RANK)
 
@@ -16,9 +16,6 @@ def clean_sjr_dataset():
             decimal=","
         )
 
-        sjr_columns = ['Rank', 'Title', 'Issn', 'SJR']
-        sjr_df = sjr_df[sjr_columns]
-
         log_dataframe(sjr_df)
 
         return sjr_df
@@ -27,6 +24,12 @@ def clean_sjr_dataset():
         print("File not found. Please make sure the file name and path are correct.")
     except Exception as e:
         print("An error occurred while reading the file:", str(e))
+
+def filter_sjr_columns(sjr_df):
+    sjr_columns = ['Rank', 'Title', 'Issn', 'SJR']
+
+    sjr_df = sjr_df[sjr_columns]
+    return sjr_df
 
 
 def count_issn_lengths(sjr_df):
@@ -89,38 +92,16 @@ def write_cleaned_impact_factor(sjr_df):
 
     sjr_df.to_csv(cleaned_impact_factor_path, index=False)
 
+def process_sjr_impact_factor():
+    sjr_df = read_sjr_dataset() # NaN Count: SJR = 210, Rest = 0
+    sjr_df = filter_sjr_columns(sjr_df)
+    count_issn_lengths(sjr_df)
+    log_df_issn_lengths(sjr_df)
+    sjr_df = handle_sjr_issn(sjr_df)
+    count_issn_lengths(sjr_df)
+    # Write:
+    # write_cleaned_impact_factor(sjr_df)
 
-sjr_df = clean_sjr_dataset() # NaN Count: SJR = 210, Rest = 0
-count_issn_lengths(sjr_df)
-log_df_issn_lengths(sjr_df)
-sjr_df = handle_sjr_issn(sjr_df)
-count_issn_lengths(sjr_df)
-# Write:
-# write_cleaned_impact_factor(sjr_df)
+process_sjr_impact_factor()
 
-
-"""
-Example Output:
-https://results2021.ref.ac.uk/outputs/1b7d4ae7-486f-455a-bf27-d490635acef2?page=1
-where,
-title: A bijective variant of the Burrows–Wheeler Transform using V-order
-journal: Theoretical Computer Science
-issn: 0304-3975
-
-In SJR data, the row looks like this: (In column 1)
-9480;20571;"Theoretical Computer Science";journal;"03043975";0
-
-In SJR data, the headers:
-Rank;Sourceid;Title;Type;Issn;SJR;SJR Best Quartile;H index;Total Docs. (2023);Total Docs. (3years);Total Refs.;Total Cites (3years);Citable Docs. (3years);Cites / Doc. (2years);Ref. / Doc.;%Female;Overton;SDG;Country;Region;Publisher;Coverage;Categories;Areas
-
-Matching:
-9480;20571;"Theoretical Computer Science";journal;"03043975";0
-Rank;Sourceid;Title;Type;Issn;
-
-remaining fields:
-SJR;SJR Best Quartile;H index;Total Docs. (2023);Total Docs. (3years);Total Refs.;Total Cites (3years);Citable Docs. (3years);Cites / Doc. (2years);Ref. / Doc.;%Female;Overton;SDG;Country;Region;Publisher;Coverage;Categories;Areas
-"""
-
-# TODO
-# Move Join to another file
-# Initial data investigation -> can plot things etc (in first file)
+# TODO Initial data investigation -> can plot things etc (in first file)
