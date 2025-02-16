694d03d2cdc2245a1828d09341fa284fb3faca8f
jadenpinto <jadenclivepinto@gmail.com>
2025-02-16 02:07:05 +0000

    log_failed_joins() to log records where ISSN is null, or valid with empty SJR

--- scripts/join_outputs_impact.py
+++ scripts/join_outputs_impact.py
@@ -28,47 +28,51 @@ def join_journal_article_metadata_with_sjr():
     journal_article_metadata = get_journal_article_metadata(cs_outputs_df)
     sjr_impact_df = get_sjr_impact_factor_df()
 
-
     log_dataframe(journal_article_metadata) # Shape: 5573x40
-    log_dataframe(sjr_impact_df)            # Shape: 46877x5
+    log_dataframe(sjr_impact_df)            # Shape: 46877x4
 
     # Join: journal_article_metadata [ISSN], sjr_impact_df ['Issn']
     joined_df = pd.merge(
         journal_article_metadata, sjr_impact_df, left_on='ISSN', right_on='Issn', how='left'
     )
-    log_dataframe(joined_df)               # Shape: 5573x45
+    log_dataframe(joined_df)               # Shape: 5573x44
+    return joined_df
 
-    # Failed joins: Issn = None
-    print(joined_df.isna().sum()) # Issn = 313. 313 failed joins. [successful joins = 5573-313 = 5260]
+def log_failed_joins(sjr_joined_df):
+    # Failed joins: Records where Issn = None
+    failed_join_count = sjr_joined_df['Issn'].isna().sum() #  313 failed joins. [successful joins = 5573-313 = 5260]
+    print(f"Count of output records whose ISSN could not be found in journal metadata = {failed_join_count}")
 
-    # Issn is Valid but SJR is None
-    joins_with_no_sjr = joined_df[
-        ~joined_df['Issn'].isnull() & joined_df["SJR"].isna()
+    # Invalid SJR: Issn is Valid (successful join) but SJR is None
+    joins_with_no_sjr = sjr_joined_df[
+        ~sjr_joined_df['Issn'].isnull() & sjr_joined_df["SJR"].isna()
     ]
-    print("SHAPE:")
-    print(joins_with_no_sjr.shape) # 4x5 (4 such records where Issn is Valid but SJR is None)
-    print(joins_with_no_sjr.to_string())
+    print(f"Number of output records with successful ISSN joins but invalid SJR = {joins_with_no_sjr.shape[0]}")
+    print(joins_with_no_sjr.to_string()) # 4x44 (4 such records where Issn is Valid but SJR is None)
 
 def join_outputs_metadata_with_sjr():
     cs_outputs_df = get_cs_outputs_metadata()
     sjr_impact_df = get_sjr_impact_factor_df()
 
     log_dataframe(cs_outputs_df)            # Shape: 7296x40
-    log_dataframe(sjr_impact_df)            # Shape: 46877x5
+    log_dataframe(sjr_impact_df)            # Shape: 46877x4
 
     joined_df = pd.merge(
         cs_outputs_df, sjr_impact_df, left_on='ISSN', right_on='Issn', how='left'
     )
-    log_dataframe(joined_df)               # Shape: 7296x45
+    log_dataframe(joined_df)               # Shape: 7296x44
 
     # Failed joins: Issn = None
     print(joined_df.isna().sum()) # Issn = 1665. 1665 failed joins. [successful joins = 7296-1665 = 5631]
     # So, besides journal articles, a few entries of other types have an SJR impact [5631 - 5260 = 371]
     # I could visualise this, to see, of all records that were successfully joined, plot pie chart of output type
 
-join_journal_article_metadata_with_sjr()
-join_outputs_metadata_with_sjr()
+    return joined_df
 
+journal_metadata_with_sjr = join_journal_article_metadata_with_sjr()
+outputs_with_sjr = join_outputs_metadata_with_sjr()
+
+log_failed_joins(journal_metadata_with_sjr)
 
 """
 Also now that I had split up records earlier
