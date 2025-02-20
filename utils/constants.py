d6c7ce58a311864c6b4fe9cb45f133d794eb98c8
jadenpinto <jadenclivepinto@gmail.com>
2025-02-20 02:10:02 +0000

    Move output and result CSVs into raw and processed folders

--- utils/constants.py
+++ utils/constants.py
@@ -2,13 +2,11 @@
 DATASETS_DIR = "datasets"
 RAW_DIR = "raw"
 PROCESSED_DIR = "processed"
-REF2021_EXPORTED_DIR = "REF2021_Exported"
-PROCESSED_REF2021_DIR = "REF2021_Cleaned"
 
 # Files
-CS_UOA_RESULTS =  "CS_UoA_Results.xlsx"
-OUTPUTS_METADATA = "All_Outputs_Metadata.xlsx"
-CS_OUTPUTS_METADATA = "CS_Outputs_Metadata.csv"
+CS_RESULTS =  "REF2021_CS_Results.xlsx"
+OUTPUTS_METADATA = "REF2021_Outputs_Metadata.xlsx"
+CS_OUTPUTS_METADATA = "REF2021_CS_Outputs_Metadata.csv"
 SCIMAGO_JOURNAL_RANK = "SCImago_Journal_Rank.csv"
 
 # Output Metadata
