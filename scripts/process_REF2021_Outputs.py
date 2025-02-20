aacf1a4e839957b1199aff2d626761f0299104ff
jadenpinto <jadenclivepinto@gmail.com>
2025-02-20 02:15:09 +0000

    Update file for preprocessing ref2021 output metadata

---
+++ scripts/process_REF2021_Outputs.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+
+from utils.constants import DATASETS_DIR, RAW_DIR, OUTPUTS_METADATA, PROCESSED_DIR, CS_OUTPUTS_METADATA
+from utils.dataframe import log_dataframe
+
+
+def read_ref_outputs():
+    outputs_dataset_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, RAW_DIR, OUTPUTS_METADATA)
+
+    try:
+        # Load the Excel file, skipping the first 4 lines -> 4th line will be used as the header
+        ref_outputs_df = pd.read_excel(outputs_dataset_path, skiprows=4)
+        return ref_outputs_df
+
+    except FileNotFoundError:
+        print("File not found. Please make sure the file name and path are correct.")
+    except Exception as e:
+        print("An error occurred while reading the file:", str(e))
+
+def filter_cs_outputs(ref_outputs_df):
+    cs_outputs = ref_outputs_df[ref_outputs_df['Unit of assessment number'] == 11]
+    cs_outputs = cs_outputs.drop(columns=['Unit of assessment number', 'Unit of assessment name'])
+
+    return cs_outputs
+
+def write_cs_outputs(cs_outputs):
+    cs_outputs_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, PROCESSED_DIR,
+                                           CS_OUTPUTS_METADATA)
+
+    cs_outputs.to_csv(cs_outputs_path, index=False)
+
+def read_cs_outputs():
+    cs_outputs_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, PROCESSED_DIR,
+                                   CS_OUTPUTS_METADATA)
+    cs_outputs_df = pd.read_csv(cs_outputs_path)
+
+    return cs_outputs_df
+
+def process_cs_outputs():
+    ref_outputs_df = read_ref_outputs()
+    cs_outputs = filter_cs_outputs(ref_outputs_df)
+    log_dataframe(cs_outputs)
+    write_cs_outputs(cs_outputs)
+
+process_cs_outputs()
+cs_outputs_df = read_cs_outputs()
+
+# How many citation counts non-applicable => All are Yes
+# c_a = df['Citations applicable'].unique()
+# for ca in c_a:
+#    print(ca)
+
+
+
+# How many don't have citation counts => 906
+# nan_count = df['Citation count'].isna().sum()
+# print("Number of records where Citation count is NaN or None:", nan_count)
+# df_nan_citations = df[df['Citation count'].isna()]
+# print(df_nan_citations.head().to_string())
+#
+# # So it's not just D (Journal Articles) that have citation counts, but only they have impact factors
+# filtered_df = df[(df['Citation count'].notna()) & (df['Output type'] != 'D')]
+# count = len(filtered_df)
+# print("Number of records where Citation count is not NaN/None and Output type is not 'D':", count) # 1138
+# print(filtered_df.head().to_string())
+
+# For conference impact factors for example, you can search for them in the CSV using ISSN & get citation counts
