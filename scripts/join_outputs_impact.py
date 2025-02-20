18fc7587cb8e44757dee7beaafc1d73897c62c77
jadenpinto <jadenclivepinto@gmail.com>
2025-02-20 01:03:45 +0000

    Update SJR ranking dataset with v2021 instead of v2023

--- scripts/join_outputs_impact.py
+++ scripts/join_outputs_impact.py
@@ -1,8 +1,7 @@
 import os
 import pandas as pd
 
-from utils.constants import (DATASETS_DIR, PROCESSED_IMPACT_FACTOR_DIR, PROCESSED_SCIMAGO_JOURNAL_RANK, PROCESSED_REF2021_DIR,
-                             CS_OUTPUTS_METADATA)
+from utils.constants import DATASETS_DIR, PROCESSED_DIR, SCIMAGO_JOURNAL_RANK, PROCESSED_REF2021_DIR, CS_OUTPUTS_METADATA
 from utils.dataframe import log_dataframe
 
 def get_cs_outputs_metadata():
@@ -17,8 +16,8 @@ def get_journal_article_metadata(cs_outputs_df):
     return journal_article_metadata
 
 def get_sjr_impact_factor_df():
-    processed_sjr_csv_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, PROCESSED_IMPACT_FACTOR_DIR,
-                                           PROCESSED_SCIMAGO_JOURNAL_RANK)
+    processed_sjr_csv_path = os.path.join(os.path.dirname(__file__), "..", DATASETS_DIR, PROCESSED_DIR,
+                                           SCIMAGO_JOURNAL_RANK)
 
     sjr_impact_df = pd.read_csv(processed_sjr_csv_path)
     return sjr_impact_df
@@ -29,7 +28,7 @@ def join_journal_article_metadata_with_sjr():
     sjr_impact_df = get_sjr_impact_factor_df()
 
     log_dataframe(journal_article_metadata) # Shape: 5573x40
-    log_dataframe(sjr_impact_df)            # Shape: 46877x4
+    log_dataframe(sjr_impact_df)            # Shape: 47524x4
 
     # Join: journal_article_metadata [ISSN], sjr_impact_df ['Issn']
     joined_df = pd.merge(
@@ -40,7 +39,7 @@ def join_journal_article_metadata_with_sjr():
 
 def log_failed_joins(sjr_joined_df):
     # Failed joins: Records where Issn = None
-    failed_join_count = sjr_joined_df['Issn'].isna().sum() #  313 failed joins. [successful joins = 5573-313 = 5260]
+    failed_join_count = sjr_joined_df['Issn'].isna().sum() #  303 failed joins. [successful joins = 5573-303 = 5270]
     print(f"Count of output records whose ISSN could not be found in journal metadata = {failed_join_count}")
 
     # Invalid SJR: Issn is Valid (successful join) but SJR is None
@@ -48,14 +47,14 @@ def log_failed_joins(sjr_joined_df):
         ~sjr_joined_df['Issn'].isnull() & sjr_joined_df["SJR"].isna()
     ]
     print(f"Number of output records with successful ISSN joins but invalid SJR = {joins_with_no_sjr.shape[0]}")
-    print(joins_with_no_sjr.to_string()) # 4x44 (4 such records where Issn is Valid but SJR is None)
+    print(joins_with_no_sjr.to_string()) # 14x44 (14 such records where Issn is Valid but SJR is None)
 
 def join_outputs_metadata_with_sjr():
     cs_outputs_df = get_cs_outputs_metadata()
     sjr_impact_df = get_sjr_impact_factor_df()
 
     log_dataframe(cs_outputs_df)            # Shape: 7296x40
-    log_dataframe(sjr_impact_df)            # Shape: 46877x4
+    log_dataframe(sjr_impact_df)            # Shape: 47524x4
 
     joined_df = pd.merge(
         cs_outputs_df, sjr_impact_df, left_on='ISSN', right_on='Issn', how='left'
@@ -63,8 +62,8 @@ def join_outputs_metadata_with_sjr():
     log_dataframe(joined_df)               # Shape: 7296x44
 
     # Failed joins: Issn = None
-    print(joined_df.isna().sum()) # Issn = 1665. 1665 failed joins. [successful joins = 7296-1665 = 5631]
-    # So, besides journal articles, a few entries of other types have an SJR impact [5631 - 5260 = 371]
+    print(joined_df.isna().sum()) # Issn = 1641. 1641 failed joins. [successful joins = 7296-1641 = 5655]
+    # So, besides journal articles, a few entries of other types have an SJR impact [5655 - 5270 = 385]
     # I could visualise this, to see, of all records that were successfully joined, plot pie chart of output type
 
     return joined_df
@@ -93,4 +92,18 @@ SJR:
 Security and Communication Network -> 0.494 (as of 2022)
 Wireless Communications and Mobile Computing -> 0.445 (as of 2022)
 Advanced Intelligent Systems -> No Data
+"""
+
+"""
+REF Timetable (https://2021.ref.ac.uk/about-the-ref/timetable/index.html)
+- 31 December 2020: End of publication period (cut-off point for publication of research outputs)
+- May 2021 – February 2022 Panels assess submissions
+
+So it's fair to assume most were assessed in 2021
+downloaded SJR data for 2021.
+
+Use the score for the year in which the assessment was carried out on the outputs
+
+raw/SCImago_Journal_Rank
+processed in processed: processed/SCImago_Journal_Rank
 """
