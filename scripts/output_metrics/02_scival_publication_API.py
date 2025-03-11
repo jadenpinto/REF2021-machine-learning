213352e1019a16b177987b9f103be9a71c46fea6
jadenpinto <jadenclivepinto@gmail.com>
2025-03-11 02:12:02 +0000

    Add function to parse api response json to get citation and view metadata of output

--- scripts/output_metrics/02_scival_publication_API.py
+++ scripts/output_metrics/02_scival_publication_API.py
@@ -48,16 +48,66 @@ def get_output_metadata(scopus_id): # FieldWeightedCitationImpact & OutputsInTop
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
 
+def extract_output_metadata(data):
+    try:
+        if not data:
+            return {}
 
+        extracted_metadata = {
+            "field_weighted_citation_impact": None,
+            "top_citation_percentile": None, # Here, null implies 100 (in the top 100% citation percentile)
+            "field_weighted_views_impact": None
+        }
 
-def process_citation_metrics():
+        json_results = data.get("results", [{}])
+        json_result = json_results[0] # We only query for one document at a time
+        metrics = json_result.get("metrics", [])
+
+        for metric in metrics:
+
+            # Extract Field Weighted Citation Impact
+            if metric.get("metricType") == "FieldWeightedCitationImpact":
+                extracted_metadata["field_weighted_citation_impact"] = metric.get("value")
+
+            # Extract Top Citation Percentile
+            elif metric.get("metricType") == "OutputsInTopCitationPercentiles":
+                percentile_values = metric.get("values", [])
+
+                for is_top_percentile in percentile_values:
+                    if is_top_percentile.get("value") == 1:
+                        extracted_metadata["top_citation_percentile"] = is_top_percentile.get("threshold")
+                        break
+
+            # Extract Field Weighted Views Impact
+            elif metric.get("metricType") == "FieldWeightedViewsImpact":
+                extracted_metadata["field_weighted_views_impact"] = metric.get("value")
+
+        return extracted_metadata
+
+    except (IndexError, KeyError, TypeError, ValueError) as e:
+        # Handle potential JSON decoding errors
+        print(f"Error decoding JSON response body: {e}")
+        return None
+
+def process_output_metrics():
     cs_scopus_id_df = get_cs_scopus_id_df()
+
+    def to_do_func():
+        return {}
+
+    cs_output_metrics_df = cs_scopus_id_df["scopus_id"].apply(to_do_func).apply(pd.Series)
+
+    # for scopus_id in cs_scopus_id_df['scopus_id'].head():
+    #     print(scopus_id)
+    #     #data = get_output_metadata(scopus_id)
+    #     #extract = extract_output_metadata(data)
+    #     #print(extract)
+
 
 configure()
 elsevier_api_key = os.getenv('elsevier_api_key')
 
-process_citation_metrics()
-
+process_output_metrics()
 
 
 
