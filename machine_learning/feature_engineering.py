aa89c9436dae4736728fd1a64ffb3af4586015d4
jadenpinto <jadenclivepinto@gmail.com>
2025-04-16 16:23:15 +0100

    Function to test skewness of total citations including a plot of col

--- machine_learning/feature_engineering.py
+++ machine_learning/feature_engineering.py
@@ -1,7 +1,11 @@
+import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import skew
 
 from machine_learning.cs_output_results import get_cs_outputs_enriched_metadata
+from utils.constants import FIGURES_DIR
 
 
 def main():
@@ -12,9 +16,46 @@ def main():
 
     print(cs_outputs_enriched_metadata.dtypes)
 
+    check_skewness_total_citations(cs_outputs_enriched_metadata)
+
     a = transform_and_normalise_citations(cs_outputs_enriched_metadata)
     print(a['normalised_citations'].describe())
 
+def check_skewness_total_citations(cs_outputs_enriched_metadata):
+    """
+    Function to test if the total citations feature is skewed
+    :param cs_outputs_enriched_metadata: Dataframe containing the total_citations field
+    :return: None
+    """
+    # Drop all nulls from total citations
+    total_citations_col = cs_outputs_enriched_metadata['total_citations'].dropna()
+    total_citations_col_skew = skew(total_citations_col)
+
+    # Plot histogram:
+    plt.hist(total_citations_col, bins=300)
+    plt.title(f'Distribution of Total Citations \nSkewness = {total_citations_col_skew}')
+    plt.xlabel('Total Citations')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+
+    # Save plot:
+    total_citations_skew_test_path = os.path.join(
+        os.path.dirname(__file__), "..", FIGURES_DIR, "total_citations_skew_test.png"
+    )
+    plt.savefig(total_citations_skew_test_path)
+
+    plt.show()
+
+    # Check if the feature is skewed based on its skew value
+    # Interpreting results of scipy skew metric:
+    if total_citations_col_skew > 1:
+        print(f"total_citations is highly positively skewed - Right-skewed (tail on the right).")
+    elif total_citations_col_skew < -1:
+        print(f"total_citations is highly negatively skewed - Left-skewed (tail on the left)")
+    elif -0.5 < total_citations_col_skew < 0.5:
+        print(f"total_citations is symmetrical.") # Near zero skew implies Symmetric distribution
+    else:
+        print(f"total_citations is skewed (moderate).")
 
 def transform_and_normalise_citations(df):
     # Create a copy - avoid modifying the original dataframe
