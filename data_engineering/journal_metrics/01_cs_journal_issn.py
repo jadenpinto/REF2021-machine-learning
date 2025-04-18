25ba77d62d9236b1e79438e8cb994f6fd927cac2
jadenpinto <jadenclivepinto@gmail.com>
2025-04-18 19:13:41 +0100

    issn - add main function

--- data_engineering/journal_metrics/01_cs_journal_issn.py
+++ data_engineering/journal_metrics/01_cs_journal_issn.py
@@ -3,6 +3,9 @@ import os
 from utils.REF2021_Outputs import get_cs_journal_article_metadata
 from utils.constants import DATASETS_DIR, PROCESSED_DIR, CS_JOURNALS_ISSN
 
+def main():
+    process_cs_journal_ISSN()
+
 def get_cs_journals_issn_df():
     cs_journal_article_metadata = get_cs_journal_article_metadata()
     cs_journal_ISSN_df = cs_journal_article_metadata[["ISSN"]].drop_duplicates().dropna()
@@ -18,4 +21,5 @@ def process_cs_journal_ISSN():
     cs_journal_ISSN_df = get_cs_journals_issn_df()
     write_cs_journals_issn_df(cs_journal_ISSN_df)
 
-process_cs_journal_ISSN()
+if __name__ == "__main__":
+    main()
