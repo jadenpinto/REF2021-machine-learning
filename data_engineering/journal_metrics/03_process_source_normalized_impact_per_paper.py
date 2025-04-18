f67811a59d8a334d49cb0217f2d07cfd45b38bba
jadenpinto <jadenclivepinto@gmail.com>
2025-04-18 23:30:55 +0100

    SNIP - add comments

--- data_engineering/journal_metrics/03_process_source_normalized_impact_per_paper.py
+++ data_engineering/journal_metrics/03_process_source_normalized_impact_per_paper.py
@@ -4,11 +4,18 @@ import pandas as pd
 from utils.constants import DATASETS_DIR, PROCESSED_DIR, RAW_DIR, SOURCE_NORMALIZED_IMPACT_PER_PAPER, SNIP
 
 def main():
+    """
+    ETL pipeline for the SNIP dataset (used to fill-in values that are missing after API call)
+    """
     snip_df = load_snip_df()
     processed_snip_df = process_snip_df(snip_df)
     write_snip_df(processed_snip_df)
 
 def load_snip_df():
+    """
+    Read in the CWTS Journal metrics file as a pandas dataframe
+    :return: Pandas dataframe of CWTS journal metrics including SNIP
+    """
     snip_dataset_path = os.path.join(os.path.dirname(__file__), "..", "..", DATASETS_DIR, RAW_DIR,
                                           SOURCE_NORMALIZED_IMPACT_PER_PAPER)
 
@@ -16,6 +23,12 @@ def load_snip_df():
     return snip_df
 
 def process_snip_df(raw_snip_df):
+    """
+    Filter CWTS Journal metrics for 2021 values of SNIP. Normalise data to 1NF by splitting each record (which has
+    both electronic and print ISSN) into separate records with one ISSN per row.
+    :param raw_snip_df: Pandas dataframe of CWTS journal metrics including SNIP
+    :return: Pandas dataframe of ISSN and SNIP values
+    """
     processed_snip_df = raw_snip_df[
         raw_snip_df["Year"] == 2021
     ]
@@ -31,6 +44,10 @@ def process_snip_df(raw_snip_df):
     return processed_snip_df
 
 def write_snip_df(snip_df):
+    """
+    Write the resulting SNIP dataframe as a parquet file
+    :param snip_df: Pandas dataframe of ISSN and SNIP values
+    """
     snip_df_path = os.path.join(os.path.dirname(__file__), "..", "..", DATASETS_DIR, PROCESSED_DIR, SNIP)
 
     snip_df.to_parquet(snip_df_path, engine='fastparquet')
