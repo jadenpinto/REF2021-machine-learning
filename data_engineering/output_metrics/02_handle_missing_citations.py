314ec49a8329b1bd00378975d5500d84eb3ab595
jadenpinto <jadenclivepinto@gmail.com>
2025-05-01 15:32:52 +0100

    missing citations - comments

--- data_engineering/output_metrics/02_handle_missing_citations.py
+++ data_engineering/output_metrics/02_handle_missing_citations.py
@@ -5,6 +5,9 @@ from utils.constants import DATASETS_DIR, PROCESSED_DIR, CS_OUTPUTS_METADATA, RE
 from utils.dataframe import split_df_on_null_field
 
 def main():
+    """
+    ETL Pipeline to fill-in the citations of outputs submitted to the CS UoA that were missing after unsuccessful API calls
+    """
     process_missing_citations()
 
 def get_cs_outputs_metadata():
@@ -16,6 +19,10 @@ def get_cs_outputs_metadata():
     return cs_outputs_metadata
 
 def load_cs_citation_metadata_df():
+    """
+    Load the file containing the citation metadata of outputs submitted to the CS UoA as a DataFrame
+    :return: DataFrame containing the citation metadata of outputs submitted to the CS UoA
+    """
     cs_citation_metadata_df_path = os.path.join(
         os.path.dirname(__file__), "..", "..", DATASETS_DIR, REFINED_DIR, CS_CITATION_METRICS
     )
@@ -23,6 +30,10 @@ def load_cs_citation_metadata_df():
     return cs_citation_metadata_df
 
 def log_missing_citations(cs_citation_metadata_df):
+    """
+    Log the count of the outputs missing citation counts
+    :param cs_citation_metadata_df: DataFrame containing the citation metadata of outputs submitted to the CS UoA
+    """
     outputs_missing_citations = cs_citation_metadata_df['total_citations'].isna().sum()
     print(f"Number of outputs missing citation counts: {outputs_missing_citations}")
 
@@ -31,12 +42,9 @@ def fill_missing_citations(cs_citation_metadata_df, cs_outputs_metadata):
     Fills missing total_citations in cs_citation_metadata_df with values from
     cs_outputs_metadata using DOI as the matching key.
 
-    Parameters:
-    cs_citation_metadata_df (pd.DataFrame): DataFrame containing total_citations and DOI.
-    cs_outputs_metadata (pd.DataFrame): DataFrame containing DOI and Citation count.
-
-    Returns:
-    pd.DataFrame: Updated cs_citation_metadata_df with filled total_citations.
+    :param cs_citation_metadata_df: DataFrame containing total_citations and DOI.
+    :param cs_outputs_metadata: DataFrame containing DOI and Citation count.
+    :return: Updated cs_citation_metadata_df with filled total_citations.
     """
 
     cs_outputs_metadata = cs_outputs_metadata.drop_duplicates(subset="DOI")
@@ -46,7 +54,8 @@ def fill_missing_citations(cs_citation_metadata_df, cs_outputs_metadata):
     This creates duplicate rows in merged_df, potentially leading to more NaN values where scopus_id was previously present.
     """
 
-    # Merge on DOI, bringing in Citation count from cs_outputs_metadata
+    # Update the CS Citation metadata DF by Merging it with cs_outputs_metadata, to obtain the metadata 'Citation count'
+    # field by joining on DOI
     merged_df = cs_citation_metadata_df.merge(
         cs_outputs_metadata[['DOI', 'Citation count']],
         on="DOI",
@@ -63,6 +72,10 @@ def fill_missing_citations(cs_citation_metadata_df, cs_outputs_metadata):
 
 
 def write_handled_missing_citations_df(cs_citation_metadata_df):
+    """
+    Write the updated cs citation metadata dataframe containing output's DOI with citation counts
+    :param cs_citation_metadata_df: DataFrame of CS outputs with their DOI with citation counts
+    """
     cs_citation_metadata_df_path = os.path.join(
         os.path.dirname(__file__), "..", "..", DATASETS_DIR, REFINED_DIR, CS_CITATION_METRICS
     )
@@ -74,6 +87,9 @@ def write_handled_missing_citations_df(cs_citation_metadata_df):
 
 
 def process_missing_citations():
+    """
+    ETL pipeline that reads in CS outputs citations file, and fills in missing values using the CS outputs metadata file
+    """
     cs_citation_metadata_df = load_cs_citation_metadata_df()
 
     log_missing_citations(cs_citation_metadata_df) # Number of outputs missing citation counts: 174
