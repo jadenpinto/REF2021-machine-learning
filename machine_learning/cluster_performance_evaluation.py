367b1ab895e0bbb5c49bedda948d58f436004ed5
jadenpinto <jadenclivepinto@gmail.com>
2025-05-01 18:04:06 +0100

    cluster metrics - comments

--- machine_learning/cluster_performance_evaluation.py
+++ machine_learning/cluster_performance_evaluation.py
@@ -5,6 +5,14 @@ from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_har
 # Internal indices - No external information, evaluate
 
 def get_cluster_evaluation_metrics(model, training_feature_array, predicted_training_labels):
+    """
+    Compute internal indices used to assess the quality of clusters
+
+    :param model: Clustering model that is fitted i.e. the collection of datapoints use to train the clustering model
+    :param training_feature_array: Array of features used to train the model
+    :param predicted_training_labels: The labels i.e. cluster assignments of the datapoints used to train the model
+    :return:
+    """
     evaluation_metrics = {}
 
     evaluation_metrics["silhouette_score"] = silhouette_score(training_feature_array, predicted_training_labels)
@@ -16,6 +24,14 @@ def get_cluster_evaluation_metrics(model, training_feature_array, predicted_trai
     return evaluation_metrics
 
 def update_total_evaluation_metrics(total_evaluation_metrics, cluster_evaluation_metrics):
+    """
+    Update the hash-map storing the sum of all internal clustering indices of all runs so far by adding the values
+    of the internal indices from the current run.
+
+    :param total_evaluation_metrics: Hash-map containing the sum of all internal clustering indices of runs so far
+    :param cluster_evaluation_metrics: Hash-map containing the internal clustering indices of the current run (fold)
+    :return: Updated hash-map containing the sum of all internal clustering indices (with current run's indices added)
+    """
     total_evaluation_metrics["total_silhouette_score"] += cluster_evaluation_metrics["silhouette_score"]
     total_evaluation_metrics["total_davies_bouldin_score"] += cluster_evaluation_metrics["davies_bouldin_score"]
     total_evaluation_metrics["total_calinski_harabasz_score"] += cluster_evaluation_metrics["calinski_harabasz_score"]
@@ -25,6 +41,12 @@ def update_total_evaluation_metrics(total_evaluation_metrics, cluster_evaluation
     return total_evaluation_metrics
 
 def compute_cluster_evaluation_metrics(total_evaluation_metrics, total_folds):
+    """
+    Compute the final internal cluster indices by averaging the indices over all folds
+
+    :param total_evaluation_metrics: Hash-map containing the sum of all internal clustering indices of runs so far
+    :param total_folds: Total number of folds (runs) where a clustering model was built and evaluated
+    """
     average_silhouette_score = total_evaluation_metrics["total_silhouette_score"] / total_folds
     average_davies_bouldin_score = total_evaluation_metrics["total_davies_bouldin_score"] / total_folds
     average_calinski_harabasz_score = total_evaluation_metrics["total_calinski_harabasz_score"] / total_folds
@@ -39,7 +61,7 @@ def compute_cluster_evaluation_metrics(total_evaluation_metrics, total_folds):
     print(f"Average Between-Cluster Sum of Squares: {average_bcss:.4f}")
     print()
 
-# External indices - Uses truth labels (proportion of high/low scoring outputs)
+# Regression metrics
 
 def compute_clustering_accuracy(
         actual_high_scoring_output_percentages,
@@ -47,8 +69,15 @@ def compute_clustering_accuracy(
         actual_low_scoring_output_percentage,
         predicted_low_scoring_output_percentage
     ):
-    # Using both high and low percentages is redundant since one can be derived from the other
-    # Instead, only use one to derive accuracy of model - High
+    """
+    Compute regression metrics by comparing the actual again the predicted number of high scoring outputs of the university
+    who's outputs are used to test the model (training set)
+
+    :param actual_high_scoring_output_percentages: Array of the actual number of high scoring outputs of the university used as test-set
+    :param predicted_high_scoring_output_percentages: Array of the predicted number of high scoring outputs of the university used as test-set
+    :param actual_low_scoring_output_percentage: Array of the actual number of low scoring outputs of the university used as test-set
+    :param predicted_low_scoring_output_percentage: Array of the predicted number of low scoring outputs of the university used as test-set
+    """
 
     actual_high = np.array(actual_high_scoring_output_percentages)
     predicted_high = np.array(predicted_high_scoring_output_percentages)
@@ -60,6 +89,7 @@ def compute_clustering_accuracy(
     rmse = np.sqrt(np.mean((actual_high - predicted_high) ** 2))
 
     # Mean Absolute Percentage Error
+    # In cases where the actual number of high scoring outputs is zero, compare using low scoring outputs instead
     mape_values = []
 
     predictions = len(predicted_high_scoring_output_percentages)
@@ -79,18 +109,27 @@ def compute_clustering_accuracy(
     # R^2 Score (Coefficient of Determination) - quantify how well the predictions fit the actual values
     r2 = r2_score(actual_high_scoring_output_percentages, predicted_high_scoring_output_percentages)
 
-    # Lower is better!
     print("Regression Scores - compare percentage of predicted and actual high scoring outputs")
+    # Lower is better
     print(f"Mean Absolute Error: {mae:.4f}")
     print(f"Root Mean Squared Error: {rmse:.4f}")
     print(f"Mean Absolute Percentage Error: {mape:.4f}")
 
     # [-inf, 1] => 1 means Perfect predictions.
     # 0 means no relation at all between predicted and actual values
+    # Higher is better
     print(f"R^2 score: {r2:.4f}")
     print()
 
+# Divergence metrics:
+
 def get_divergence_metrics(predicted_distribution, actual_distribution):
+    """
+    Compute divergence metrics to assess the accuracy of the clustering models
+    :param predicted_distribution: Predicted distribution - probability of an output being high-scoring, and low-scoring
+    :param actual_distribution: Actual distribution - probability of an output being high-scoring, and low-scoring
+    :return: Hash-map with the divergence metrics quantifying distance between the distributions
+    """
     divergence_metrics = {}
 
     divergence_metrics["kl_divergence"] = kl_divergence(predicted_distribution, actual_distribution)
@@ -100,6 +139,14 @@ def get_divergence_metrics(predicted_distribution, actual_distribution):
     return divergence_metrics
 
 def update_total_divergence_metrics(total_divergence_metrics, divergence_metrics):
+    """
+    Update the hash-map storing the sum of all divergence of all runs so far by adding the values of the divergence metrics
+    the current run (fold)
+
+    :param total_divergence_metrics: Hash-map containing the sum of all divergence metrics of runs so far
+    :param divergence_metrics: Hash-map containing the divergence metrics of the current run (fold)
+    :return:  Updated hash-map containing the sum of all divergence metrics (with current run's divergence metrics added)
+    """
     total_divergence_metrics["total_kl_divergence"] += divergence_metrics["kl_divergence"]
     total_divergence_metrics["total_js_divergence"] += divergence_metrics["js_divergence"]
     total_divergence_metrics["total_tvd"] += divergence_metrics["tvd"]
@@ -107,6 +154,13 @@ def update_total_divergence_metrics(total_divergence_metrics, divergence_metrics
     return total_divergence_metrics
 
 def compute_divergence_metrics(total_divergence_metrics, total_folds):
+    """
+    Compute and print the overall divergence metrics between the probability distributions of
+    the actual and the predicted high- and low-scoring output counts by averaging the metrics over all folds
+
+    :param total_divergence_metrics: Hash-map containing the sum of all divergence metrics of runs so far
+    :param total_folds: Total number of folds (runs) where a clustering model was built and evaluated
+    """
     average_kl_divergence = total_divergence_metrics["total_kl_divergence"] / total_folds
     average_js_divergence = total_divergence_metrics["total_js_divergence"] / total_folds
     average_tvd = total_divergence_metrics["total_tvd"] / total_folds
@@ -119,10 +173,17 @@ def compute_divergence_metrics(total_divergence_metrics, total_folds):
 
 def kl_divergence(prob_distribution_a, prob_distribution_b):
     """
-    p: predicted = [predicted high, predicted low]
-    q:    actual = [actual high,    actual low]
+    Compute the Kullback-Leibler (KL) Divergence to quantifies difference between one probability distributions from
+    another. Lower is better.
 
-    Divergence of the predicted from actual: P || Q
+    :param prob_distribution_a: A probability distribution (predicted) - includes probability of a document being high-scoring and low-scoring
+    :param prob_distribution_b: Another probability distribution (actual) - includes probability of a document being high-scoring and low-scoring
+    :return: Kullback-Leibler Divergence between the two probability distributions
+
+    A: predicted = [predicted high, predicted low]
+    B:    actual = [actual high,    actual low]
+
+    Divergence of the predicted from actual: A || B
     """
     possible_outcomes = len(prob_distribution_a)
 
@@ -140,12 +201,25 @@ def kl_divergence(prob_distribution_a, prob_distribution_b):
 
 # calculate the js divergence - symmetric
 def js_divergence(prob_distribution_a, prob_distribution_b):
+    """
+    Compute the Jensen-Shannon (JS) Divergence to quantifies differences between probability distributions. Lower is better.
+    :param prob_distribution_a: A probability distribution (predicted) - includes probability of a document being high-scoring and low-scoring
+    :param prob_distribution_b: Another probability distribution (actual) - includes probability of a document being high-scoring and low-scoring
+    :return: Jensen-Shannon Divergence between the two probability distributions
+    """
     prob_distribution_a = np.array(prob_distribution_a)
     prob_distribution_b = np.array(prob_distribution_b)
     m = 0.5 * (prob_distribution_a + prob_distribution_b)
     return 0.5 * kl_divergence(prob_distribution_a, m) + 0.5 * kl_divergence(prob_distribution_b, m)
 
 def total_variation_distance(prob_distribution_a, prob_distribution_b):
+    """
+    Compute total variation distance between probability distributions
+    Lower is better: 0 means identical distributions and 1 means maximum divergence
+    :param prob_distribution_a: A probability distribution (predicted) - includes probability of a document being high-scoring and low-scoring
+    :param prob_distribution_b: Another probability distribution (actual) - includes probability of a document being high-scoring and low-scoring
+    :return: Total Variation Distance between the two probability distributions
+    """
     prob_distribution_a = np.array(prob_distribution_a)
     prob_distribution_b = np.array(prob_distribution_b)
     return 0.5 * np.sum(np.abs(prob_distribution_a - prob_distribution_b))
